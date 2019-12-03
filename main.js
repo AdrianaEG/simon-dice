66c8ddb1448ad9685e3d78c6ece5c88296803843
AdrianaEG <adriana.ngc438@gmail.com>
2019-12-03 18:31:12 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let primero = document.querySelector('#primero');
+let segundo = document.querySelector('#segundo');
+let tercero = document.querySelector('#tercero');
+let cuarto = document.querySelector('#cuarto');
+
