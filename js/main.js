c3659fbfbdd2cfd635b7ae5fb3ba2c0dac814d9f
AdrianaEG <adriana.ngc438@gmail.com>
2019-12-05 22:00:36 -0300

    Se da funcionalidad al botón comenzar

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@ let primero = document.querySelector('#primero');
 let segundo = document.querySelector('#segundo');
 let tercero = document.querySelector('#tercero');
 let cuarto = document.querySelector('#cuarto');
+const $botonComenzar = document.querySelector('#comenzar');
+let secuenciaUsuario = [];
+let secuenciaMaquina = [];
+let movimientos = 0;
+
+bloquearCuadradoUsuario();
+
+$botonComenzar.onclick = function (event) {
+    let ronda = 0;
+    secuenciaUsuario = [];
+    secuenciaMaquina = [];
+
+    juegaMaquina();
+    
+    event.preventDefault();
+}
 
